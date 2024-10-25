721c7e7e5f09e805be6225f7e66e35aac72e0217
Guilherme Dias Araujo <guilherme.araujo97@outlook.com>
2024-10-25 19:06:02 -0300

    Update caminhao finalizado

--- obj/Debug/net8.0/Projeto Transportadora.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projeto Transportadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto Transportadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+000d847fe54ed84d0af27509df91be86aea39a0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto Transportadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto Transportadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
